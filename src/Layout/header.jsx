1b567d96412c544901dd7076f6b804e065fb01da
Ali <ialimalik99@gmail.com>
2023-03-29 12:46:55 -0400

    add styling to the image div

--- src/Layout/header.jsx
+++ src/Layout/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./Header.module.css";
-import meals from "../assets/meals.jpg"
+import meals from "../assets/meals.jpg";
 const Header = () => {
   return (
     <React.Fragment>
@@ -8,8 +8,9 @@ const Header = () => {
         <h1>Deliveroo</h1>
         <button>Cart</button>
       </header>
-      <div>
-        <img src={meals} alt="A table full of food"></img>
+      <div className={classes["main-image"]}>
+        <img src={meals} alt="A table full of food">
+        </img>
       </div>
     </React.Fragment>
   );
