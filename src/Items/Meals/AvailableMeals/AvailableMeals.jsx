73166012191ae99d777b8a06ab5b61f8ec1bd49d
Ali <ialimalik99@gmail.com>
2023-04-02 19:08:26 -0400

    Add MealItem component for AvailableMeals mapping

--- src/Items/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/Items/Meals/AvailableMeals/AvailableMeals.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import classes from "../AvailableMeals/AvailableMeals.module.css";
+import MealItem from "../MealItem/MealItem";
 import Card from "../../../Layout/Card/Card";
+import {nanoid} from "nanoid";
 
 const MEALS = [
-  { mealName: "Sushi", description: "Finest fish and veggies", price: 22.99 },
-  { mealName: "Schnitzel", description: "A german specialty!", price: 16.5 },
   {
+    id: nanoid(),
+    mealName: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: nanoid(),
+    mealName: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: nanoid(),
     mealName: "Barbecue Burger",
     description: "An American Specialty",
     price: 10.5,
@@ -18,7 +31,14 @@ const AvailableMeals = () => {
       <Card>
         <ul>
           {MEALS.map((meal) => {
-            return <li>{meal.mealName}</li>;
+            return (
+              <MealItem
+                key={meal.id}
+                name={meal.mealName}
+                description={meal.description}
+                price={meal.price}
+              ></MealItem>
+            );
           })}
         </ul>
       </Card>
--- src/Items/Meals/MealItem/MealItem.jsx
+++ src/Items/Meals/MealItem/MealItem.jsx
@@ -1,13 +1,17 @@
+import classes from "./MealItem.module.css"
 const MealItem = (props) => {
+  const {name, description} = props;
   const price = `$${props.price.toFixed(2)}`;
   return (
-    <li>
+    <li className={classes.meal}>
       <div>
-        <h3>{props}</h3>
-        <div>{description}</div>
-        <div>{price}</div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        
+      </div>
     </li>
   );
 };
