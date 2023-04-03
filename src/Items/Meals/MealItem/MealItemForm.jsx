50f18aecde1f8d92ee64bb1c12e170fbd5c90846
Ali <ialimalik99@gmail.com>
2023-04-02 23:11:17 -0400

    add styling to add button

--- src/Items/Meals/MealItem/MealItemForm.jsx
+++ src/Items/Meals/MealItem/MealItemForm.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import classes from "./MealItemForm.module.css";
 const MealItemForm = (props) => {
   const { onSubmit } = props;
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className={classes.form}>
       <label htmlFor="quantity" className="quantity-label">
         Amount
       </label>
-      <input type="number" id="quantity" min="0" />
-      <div>
-        <button>Add+</button>
-      </div>
+      <input type="number" id="quantity" min="0" className={classes.input} />
+      <button className={classes.add}>+ Add</button>
     </form>
   );
 };
