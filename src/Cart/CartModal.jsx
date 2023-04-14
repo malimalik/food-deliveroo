1c19f67c850857b0d65aa47cca36afc824cf28a2
Ali <ialimalik99@gmail.com>
2023-04-13 22:13:29 -0400

    finish adding modal

--- src/Cart/CartModal.jsx
+++ src/Cart/CartModal.jsx
@@ -1,8 +1,28 @@
-import classes from './CartModal.module.css'
+import classes from "./CartModal.module.css";
+import { Fragment } from "react";
+import { ReactDOM } from "react";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return <div className={classes.content}>{props.children}</div>;
+};
+
+
+const portalElement = document.getElementById('overlays');
 
 const CartModal = (props) => {
-    
-}
-
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<ModalOverlay>
+        {props.children}
+      </ModalOverlay>, portalElement
+      )}
+    </Fragment>
+  );
+};
 
 export default CartModal;
