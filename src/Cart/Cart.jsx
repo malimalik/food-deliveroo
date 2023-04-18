0367dd5260637f9a0868b88b127111c2227f1d57
Ali <ialimalik99@gmail.com>
2023-04-17 22:19:09 -0400

    fix outputting cart items

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import CartModal from "./CartModal";
 import classes from "./Cart.module.css";
 import { Fragment, useContext } from "react";
 import CartContext from "../store/cart-context";
+import CartItem from "./CartItem/CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -9,10 +10,24 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItems = (
-    <ul className={classes["cart-items"]}>
+  const cartItemRemove = (id) => {
+
+  }
+  const cartItemAdd = (item) => {
+
+  }
+
+ const cartItems = (
+    <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemove.bind(null, item.id)}
+          onAdd={cartItemAdd.bind(null, item)}
+        />
       ))}
     </ul>
   );
