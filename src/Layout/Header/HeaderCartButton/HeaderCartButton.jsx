d91b810560486cf3e591524cd25b4415f4569ba7
Ali <ialimalik99@gmail.com>
2023-04-18 15:41:23 -0400

    Add animation to the cart icon

--- src/Layout/Header/HeaderCartButton/HeaderCartButton.jsx
+++ src/Layout/Header/HeaderCartButton/HeaderCartButton.jsx
@@ -1,19 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "./CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import CartContext from "../../../store/cart-context"; 
+import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsBumped, setIsBtnBumped] = useState(false);
   // const { cartAmount } = props;
   const cartCtx = useContext(CartContext);
 
   // the reduce() method will accept two parameters (currNumber, item)
   const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
-      return currNumber + item.amount;
+    return currNumber + item.amount;
   }, 0);
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${btnIsBumped ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setIsBtnBumped(true);
+    setTimeout(() => {
+      setIsBtnBumped(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
