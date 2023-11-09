77eb5c04adb74aac32bf432d5422599cdc7cb3b7
Ali <ialimalik99@gmail.com>
2023-11-09 14:46:54 -0500

    post data to the backend

--- src/Cart/CartCheckout.jsx
+++ src/Cart/CartCheckout.jsx
@@ -1,8 +1,52 @@
 import classes from "./CartCheckout.module.css";
+import CartContext from "../store/cart-context";
+import { useState, useContext } from "react";
+import axios from "axios";
+import { CleaningServicesOutlined } from "@mui/icons-material";
 
 const CartCheckout = (props) => {
+  const [checkoutData, setCheckoutData] = useState({});
+  const cartCtx = useContext(CartContext);
+
+  const submissionHandler = (event) => {
+    event.preventDefault();
+    const { items, totalAmount } = cartCtx;
+    const payload = {
+      items: items,
+      total: totalAmount,
+      details: checkoutData,
+    };
+
+    try {
+      const response = axios.post(
+        "https://deliveroo-90143-default-rtdb.firebaseio.com/orders.json",
+        payload
+      );
+
+      if (response.status === 200) {
+        console.log("Successfully submitted data");
+      } else {
+        console.error("Failed to save data", response.status);
+      }
+    } catch (error) {
+      console.error("An error occurred while saving the data", error);
+    }
+
+    console.log(payload);
+  };
+
+  const changeHandler = (event) => {
+    setCheckoutData((prevData) => {
+      return {
+        ...prevData,
+        [event.target.name]: event.target.value,
+      };
+    });
+    console.log(event.target.value);
+  };
+
   return (
-    <form onSubmit={props.handleSubmit} className={classes.form}>
+    <form onSubmit={submissionHandler} className={classes.form}>
       <div className={classes.form}>
         <label htmlFor="first-name">First name</label>
         <input
@@ -10,7 +54,7 @@ const CartCheckout = (props) => {
           id="name"
           name="first-name"
           required
-          onChange={props.handleChange}
+          onChange={changeHandler}
         />
       </div>
       <div>
@@ -20,7 +64,7 @@ const CartCheckout = (props) => {
           id="last-name"
           name="last-name"
           required
-          onChange={props.handleChange}
+          onChange={changeHandler}
         />
       </div>
       <div>
@@ -30,13 +74,15 @@ const CartCheckout = (props) => {
           id="address"
           name="address"
           required
-          onChange={props.handleChange}
+          onChange={changeHandler}
         />
       </div>
 
       <div>
         <button type="submit">Confirm</button>
-        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
       </div>
     </form>
   );
